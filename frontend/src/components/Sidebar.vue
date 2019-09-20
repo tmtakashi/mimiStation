b7d5e64fc60dce0f508840c8cce883c5cb22a489
tmtakashi <tmtakashi7@gmail.com>
2019-09-20 17:41:31 +0900

    fixed text input

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -2,29 +2,43 @@
   <v-navigation-drawer app clipped>
     <v-list-item>
       <v-list-item-content>
-        <v-list-item-title class="title">AB Sections</v-list-item-title>
+        <v-list-item-title class="title">AB Loops</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item align="center">
-      <v-btn
-        color="success"
-        @click="$store.commit('addABLoop', name)"
-        class="mb-5"
-        small
-      >Add Current AB Loop</v-btn>
+    <v-list-item>
+      <v-layout row>
+        <v-flex>
+          <v-text-field
+            @keydown.enter="addABLoop"
+            v-model="ABLoopName"
+            label="Name"
+            style="float: left; width: 80%;"
+            clearable
+            solo
+          ></v-text-field>
+          <v-btn color="success" style="float: right;" @click="addABLoop" large icon>
+            <v-icon>mdi-plus-circle</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
     </v-list-item>
+
     <v-divider></v-divider>
-    <v-list-item v-for="(loop, index) in ABLoops" :key="index" two-line>
-      <v-list-item-content>
-        <v-list-item-title>{{ loop.name }}</v-list-item-title>
-        <v-list-item-subtitle>{{ toMMSS(loop.pointATime) }} - {{ toMMSS(loop.pointBTime) }}</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action>
-        <v-btn @click="$store.commit('deleteABLoop', index)" icon>
-          <v-icon>mdi-delete-circle</v-icon>
-        </v-btn>
-      </v-list-item-action>
-    </v-list-item>
+
+    <v-list dense>
+      <v-subheader>Your AB Loops</v-subheader>
+      <v-list-item v-for="(loop, index) in ABLoops" :key="index" two-line link>
+        <v-list-item-content>
+          <v-list-item-title>{{ loop.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ toMMSS(loop.pointATime) }} - {{ toMMSS(loop.pointBTime) }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn @click="$store.commit('deleteABLoop', index)" icon>
+            <v-icon>mdi-delete-circle</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
@@ -34,7 +48,7 @@ import { mapGetters } from "vuex";
 export default {
   data: function() {
     return {
-      ABLoopName: String
+      ABLoopName: ""
     };
   },
   computed: {
@@ -47,6 +61,10 @@ export default {
       var seconds = sec_num % 60;
 
       return [minutes, seconds].map(v => (v < 10 ? "0" + v : v)).join(":");
+    },
+    addABLoop: function() {
+      this.$store.commit("addABLoop", this.ABLoopName);
+      this.ABLoopName = "";
     }
   }
 };
